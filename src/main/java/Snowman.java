640d81f18ee359947e61ee0d6eb0c5f1c9e7746a
Scott Kloustin <532080+klous@users.noreply.github.com>
2022-01-22 20:47:12 -0500

    project set up

---
+++ src/main/java/Snowman.java
@@ -0,0 +1,12 @@
+public class Snowman {
+    public static void main(String[] args) {
+        // purpose of program to let the user guess letters to a secret word, one a time
+        // Users will have a limited number of guesses, program will keep track of letters that are wrong
+
+        // initially use an array to store the words, separated by comma. like: word,another,third
+        //todo: store the secret word list in a text file or CSV and read that file
+
+
+
+    }
+}
